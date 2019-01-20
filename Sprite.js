8469ed20b663190b6bcf40f650e3816844dbd4c5
Aquino <46411240+MrNoc@users.noreply.github.com>
2019-01-20 16:58:56 -0200

    Update Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -10,10 +10,10 @@ function Sprite (x, y, largura, altura){
 }
 
 var bg = new Sprite(0, 0, 600, 600),
-spriteBoneco = new Sprite(624, 20, 84, 54),
+spriteBoneco = new Sprite(983, 17, 58, 53),
 
 perdeu = new Sprite(726, 393, 413, 160),
-jogar = new Sprite(798, 98, 186, 87),
+jogar = new Sprite(1079, 0, 319, 145),
 novo = new Sprite(603, 127, 397, 347),
 spriteRecord = new Sprite(918, 491, 257, 214),
-spriteChao = new Sprite(0, 598, 600, 100);
+spriteChao = new Sprite(0, 620, 600, 100);
